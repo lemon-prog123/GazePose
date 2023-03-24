82e6c97b75c78e30af61d00112a8dfa7bd1f7a26
lemon-prog123 <2000013085@stu.pku.edu.cn>
2023-03-24 21:30:34 +0800

    First Change

--- basetrain.sh
+++ basetrain.sh
@@ -2,4 +2,17 @@
 
 export CUDA_VISIBLE_DEVICES=9
 
-nohup python -u run.py   --Gaussiandrop  --backbone resnet18 --split 1 --batch_size 64  --lr 5e-5  --checkpoint /data/shared/weixiyu/social-interactions/checkpoints/gaze360_model.pth --num_encoder_layers 6 --num_decoder_layers 6 --dim_feedforward 2048 --val_stride 13  --train_stride 3  --model GazeLSTM --num_workers 3  --train_path trainlog/output_gazeres18train  --exp_path validationlog/output_gazeres18 >gazeres18.out &
+nohup python -u run.py
+--Gaussiandrop
+--backbone resnet18
+--split 1
+--batch_size 64
+--lr 5e-5
+--checkpoint /data/shared/weixiyu/social-interactions/checkpoints/gaze360_model.pth
+--val_stride 13
+--train_stride 3
+--model GazeLSTM
+--num_workers 3
+--train_path trainlog/output_gazeres18train
+--exp_path validationlog/output_gazeres18
+>gazeres18.out &
